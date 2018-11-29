51e39cd0178fca38a247130d4dc1b4ede986c08f
Hyunjoon Kim <joonkim0625@gmail.com>
2018-11-29 16:28:24 +0900

    로그인 기능 추가

--- src/components/LoginFormView.js
+++ src/components/LoginFormView.js
@@ -1,12 +1,59 @@
 import React, { Component } from 'react';
 
+// 1. 제어되는 함수로 만들기
+// 2. 로그인 버튼 눌렀을 때 onLogin 함수 작동
 export default class LoginFormView extends Component {
+  static defaultProps = {
+    onLoginButton: () => {},
+  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      password: '',
+    };
+  }
+
+  handleUsernameChange(e) {
+    this.setState({
+      username: e.target.value,
+    });
+  }
+  handlePasswordChange(e) {
+    this.setState({
+      password: e.target.value,
+    });
+  }
+  // 아니면 위의 두개 합치면
+  // handleFieldChange(e, name) {
+  //   this.setState({
+  //     [name]: e.target.value,
+  //   });
+  // }
+
+  // LoginForm.js에서 받은 prop을 받아와서 실행시킬 수 있게 하는 함수...
+  handleLoginButtonClick() {
+    const { onLogin } = this.props;
+    const { username, password } = this.state;
+    onLogin(username, password);
+  }
   render() {
+    const { username, password } = this.state;
+
     return (
       <div>
-        <input type="text" />
-        <input type="password" />
-        <button>로그인</button>
+        <input
+          type="text"
+          value={username}
+          onChange={e => this.handleUsernameChange(e)}
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={e => this.handlePasswordChange(e)}
+        />
+        <button onClick={() => this.handleLoginButtonClick()}>로그인</button>
       </div>
     );
   }
--- src/containers/LoginForm.js
+++ src/containers/LoginForm.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import LoginFormView from '../components/LoginFormView';
+import { withUser } from '../contexts/UserContext';
 
-export default class LoginForm extends Component {
+class LoginForm extends Component {
   render() {
-    return <LoginFormView />;
+    // 유저에서 로그인 내려주니까! 로그인 시킬 때 서버전송 하는 거 prop
+    const { login } = this.props;
+    return <LoginFormView onLogin={login} />;
   }
 }
+
+export default withUser(LoginForm);
