99d960a41a3af6c0ba45e41f9825388d20734ab3
Hyunjoon Kim <joonkim0625@gmail.com>
2018-11-29 15:19:53 +0900

    장바구니 가격 출력

--- src/components/ProductDetailView.js
+++ src/components/ProductDetailView.js
@@ -17,6 +17,30 @@ class ProductDetailView extends Component {
       // }
     ],
   };
+
+  constructor(props) {
+    super(props);
+
+    // 장바구니에 정보 전송 시, option의 id값이 필요하기 때문에
+    this.state = {
+      quantity: 1,
+      selectedOptionId: '',
+    };
+  }
+
+  handleOptionChange(e) {
+    this.setState({
+      selectedOptionId: parseInt(e.target.value),
+      quantity: 1,
+    });
+  }
+
+  handleQuantityChange(e) {
+    this.setState({
+      quantity: parseInt(e.target.value),
+    });
+  }
+
   render() {
     const {
       id,
@@ -26,13 +50,36 @@ class ProductDetailView extends Component {
       detailImgUrls,
       options,
     } = this.props;
+
+    const { quantity, selectedOptionId } = this.state;
+    const selectedOption = options.find(o => o.id === selectedOptionId);
+    // selectedOption이 있으면 &&의 뒤가 실행되고 없으면 &&의 앞인 falsy(undefined)가 반환된다.
+    const totalPrice = selectedOption && selectedOption.price * quantity;
+
     return (
       <div>
-        <select>
+        <select
+          value={selectedOptionId}
+          onChange={e => this.handleOptionChange(e)}
+          required
+        >
+          <option disabled value="">
+            옵션을 선택하세요
+          </option>
           {options.map(o => (
-            <option value={o.id}>{o.title}</option>
+            <option key={o.id} value={o.id}>
+              {o.title}
+            </option>
           ))}
         </select>
+        <input
+          type="number"
+          value={quantity}
+          min="1"
+          onChange={e => this.handleQuantityChange(e)}
+        />
+        <div>총 가격: {totalPrice}</div>
+
         <div>{id}</div>
         <div>{title}</div>
         <div>{description}</div>
